28de47e50b728ec2527380ada4d25cf77e87be78
YOUNGWOOK <YOUNGWOOK@DESKTOP-R32BTN2>
2020-08-23 16:46:10 +0900

    test

--- src/main/java/com/project/shop/board/controller/BoardController.java
+++ src/main/java/com/project/shop/board/controller/BoardController.java
@@ -5,22 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class BoardController {
-	
-	
-	// 공지사항 이동
+	// 怨듭��궗�빆 �씠�룞
 	@RequestMapping(value = "/noticeView.do")
 	public ModelAndView noticeView(ModelAndView mv) {
 		mv.setViewName("board/notice");
 		return mv;
 	}
-	
-	
-	// Qna 게시판 이동
+	// Qna 寃뚯떆�뙋 �씠�룞
 	@RequestMapping(value = "QnaView.do")
 	public ModelAndView qnaView(ModelAndView mv) {
 		mv.setViewName("board/qna");
 		return mv;
 	}
-	
-
 }
